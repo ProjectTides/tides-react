c09e84349b7bded302326c315913cc563a45b254
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-05-29 14:51:04 +0530

    extended login functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
 import LoginForm from './Containers/LoginForm/LoginForm';
+import { Redirect } from "react-router-dom";
 
 const App = () => {
+  let user = null;
+  if (user) {
+    return <Redirect to="/dashboard" />;
+  }
   return (
     <div className={styles.App}>
       <h1>Welcome to Project Tides</h1>
--- src/Containers/LoginForm/LoginForm.js
+++ src/Containers/LoginForm/LoginForm.js
@@ -2,9 +2,21 @@ import React from 'react';
 import styles from './LoginForm.module.css';
 
 const LoginForm = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // if (!validCredentials(email, password)) {
+    //   return false;
+    // }
+
+    console.log(e.target.email.value);
+    console.log(e.target.password.value);
+  }
+
   return (
-    <div class={styles.LoginForm}>
-      <div className={styles.FormWrapper}>
+    <div className={styles.LoginForm}>
+      <form className={styles.FormWrapper} onSubmit={handleSubmit}>
         <div className={styles.InputWrapper}>
           <label htmlFor="email">Email</label>
           <input type="email" name="email" className={styles.FormControl} />
@@ -14,9 +26,9 @@ const LoginForm = () => {
           <input type="password" name="password" className={styles.FormControl} />
         </div>
         <div className={styles.InputWrapper}>
-          <button type="button" className={styles.LoginButton}>Login</button>
+          <button type="submit" className={styles.LoginButton}>Login</button>
         </div>
-      </div>
+      </form>
       <div className={styles.forgotPasswordWrapper}>
         <a href="#" className={styles.forgotPassword}>Forgot password?</a>
       </div>
