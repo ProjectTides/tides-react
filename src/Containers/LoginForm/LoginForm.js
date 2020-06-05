f52546889cc1d941c2c442a243d70bdf8f3321df
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-06-05 14:24:22 +0530

    formatting

--- src/Containers/LoginForm/LoginForm.js
+++ src/Containers/LoginForm/LoginForm.js
@@ -17,72 +17,72 @@ const LOGIN_USER_VIA_EMAIL = gql`
 
 const LoginForm = () => {
 
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loginError, setLoginError] = useState(false);
-  const [loginUserViaEmail] = useMutation(LOGIN_USER_VIA_EMAIL);
+	const [authenticated, setAuthenticated] = useState(false);
+	const [loginError, setLoginError] = useState(false);
+	const [loginUserViaEmail] = useMutation(LOGIN_USER_VIA_EMAIL);
 
-  const verifyUser = (res) => {
-    console.log("data");
-    console.log(res.data);
-    const userDetails = res.data.loginUserViaEmail;
-    if (!userDetails) {
-      // show error and return false;
-      setLoginError(true);
-      return false;
-    }
-    setAuthenticated(true);
-  }
+	const verifyUser = (res) => {
+		console.log("data");
+		console.log(res.data);
+		const userDetails = res.data.loginUserViaEmail;
+		if (!userDetails) {
+			// show error and return false;
+			setLoginError(true);
+			return false;
+		}
+		setAuthenticated(true);
+	}
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    // add form validation
+		// add form validation
 
-    // make API call to authenticate
+		// make API call to authenticate
 
-    loginUserViaEmail({
-      variables: {
-        email: e.target.email.value,
-        password: e.target.password.value,
-      }
-    }).then(
-      res => verifyUser(res),
-    );
-  }
+		loginUserViaEmail({
+			variables: {
+				email: e.target.email.value,
+				password: e.target.password.value,
+			}
+		}).then(
+			res => verifyUser(res),
+		);
+	}
 
-  if (authenticated) {
-    return <Redirect to="/dashboard" />;
-  }
+	if (authenticated) {
+		return <Redirect to="/dashboard" />;
+	}
 
-  return (
-    <div className={styles.Page}>
-      <h1>Welcome to Project Tides</h1>
-      <div className={styles.LoginForm}>
-        {loginError &&
-          <div className={styles.LoginError}>
-            Invalid user name or password
+	return (
+		<div className={styles.Page}>
+			<h1>Welcome to Project Tides</h1>
+			<div className={styles.LoginForm}>
+				{loginError &&
+					<div className={styles.LoginError}>
+						Invalid user name or password
           </div>
-        }
-        <form className={styles.FormWrapper} onSubmit={handleSubmit}>
-          <div className={styles.InputWrapper}>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" className={styles.FormControl} />
-          </div>
-          <div className={styles.InputWrapper}>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" className={styles.FormControl} />
-          </div>
-          <div className={styles.InputWrapper}>
-            <button type="submit" className={styles.LoginButton}>Login</button>
-          </div>
-        </form>
-        <div className={styles.forgotPasswordWrapper}>
-          <a href="#" className={styles.forgotPassword}>Forgot password?</a>
-        </div>
-      </div>
+				}
+				<form className={styles.FormWrapper} onSubmit={handleSubmit}>
+					<div className={styles.InputWrapper}>
+						<label htmlFor="email">Email</label>
+						<input type="email" name="email" className={styles.FormControl} />
+					</div>
+					<div className={styles.InputWrapper}>
+						<label htmlFor="password">Password</label>
+						<input type="password" name="password" className={styles.FormControl} />
+					</div>
+					<div className={styles.InputWrapper}>
+						<button type="submit" className={styles.LoginButton}>Login</button>
+					</div>
+				</form>
+				<div className={styles.forgotPasswordWrapper}>
+					<a href="#" className={styles.forgotPassword}>Forgot password?</a>
+				</div>
+			</div>
 
-    </div>
-  );
+		</div>
+	);
 }
 
 export default LoginForm;
