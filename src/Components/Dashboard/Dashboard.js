62cc15b4d4e5b5d5fe15c2b18104855ed6c981ad
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-06-09 19:02:22 +0530

    messages creation

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,52 +2,95 @@ import React, { useState } from 'react';
 import styles from './Dashboard.module.css';
 import Navbar from '../Navbar/Navbar';
 import Message from '../../Components/Message/Message';
+import { useQuery, useLazyQuery, useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const CREATE_MESSAGE = gql`
+	mutation createMessage($input: MessageInput!) {
+		createMessage(input: $input) {
+			message {
+				id
+				body
+				flow
+			}
+			errors {
+				key
+				message
+			}
+		}
+	}
+`;
+
 
 const defaultMessages = [
-  {
-    type: 'received',
-    content: 'hello',
-  },
-  {
-    type: 'sent',
-    content: 'Hello! Welcome to Tides!',
-  }
+	{
+		body: 'hello',
+		senderId: 2,
+		recipientId: 1,
+		type: 'TEXT',
+	},
+	{
+		body: 'Hello! Welcome to Tides!',
+		senderId: 1,
+		recipientId: 2,
+		type: 'TEXT',
+	}
 ];
 
 const Dashboard = () => {
 
-  const [messages, setMessages] = useState(defaultMessages);
+	const [messages, setMessages] = useState(defaultMessages);
+	const [createMessage] = useMutation(CREATE_MESSAGE, {
+		update(cache, { data: { createMessage } }) {
+			// const messages: any = cache.readQuery({ query: GET_MESSAGES });
+			// cache.writeQuery({
+			// 	query: GET_MESSAGES,
+			// 	data: { messages: messages.messages.concat(createMessage.message) },
+			// });
+		},
+	});
+	const userId = 1;
 
-  const sendMessage = (e) => {
-    e.preventDefault();
-    setMessages([
-      ...messages,
-      {
-        type: 'sent',
-        content: e.target.content.value,
-      }
-    ]);
-    e.target.content.value = '';
-  }
+	const sendMessage = (e) => {
+		e.preventDefault();
+		const payload = {
+			body: e.target.content.value,
+			recipientId: 2,
+			senderId: 1,
+			type: "TEXT",
+			// flow: "OUTBOUND",
+			// waStatus: "DELIVERED"
+		}
+		setMessages([
+			...messages,
+			payload
+		]);
+		createMessage({
+			variables: {
+				input: payload
+			}
+		});
+		e.target.content.value = '';
+	}
 
-  return (
-    <div>
-      <Navbar />
-      {/* <Sidebar /> */}
-      <div className={styles.MessagesFrame}>
-        {messages.map((message, index) => {
-          if (message.type === 'sent') {
-            return <Message key={index} message={message} />;
-          }
-          return <Message key={index} message={message} />;
-        })}
-        <form className={styles.MessageSendForm} onSubmit={sendMessage}>
-          <input type="text" name="content" placeholder="Type a message..." className={styles.FormControl} />
-          <button type="submit" className={styles.SendButton}>&#10140;</button>
-        </form>
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			{/* <Sidebar /> */}
+			<div className={styles.MessagesFrame}>
+				{messages.map((message, index) => {
+					if (message.senderId === userId) {
+						return <Message key={index} message={message} userId={userId} />;
+					}
+					return <Message key={index} message={message} userId={userId} />;
+				})}
+				<form className={styles.MessageSendForm} onSubmit={sendMessage}>
+					<input type="text" name="content" placeholder="Type a message..." className={styles.FormControl} />
+					<button type="submit" className={styles.SendButton}>&#10140;</button>
+				</form>
+			</div>
+		</div>
+	)
 }
 
 export default Dashboard;
